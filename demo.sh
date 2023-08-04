77c4d808bb7aea07149efc8049ec6d32a211217a
chenkai <chenkaic4@gmail.com>
2023-08-04 16:13:46 +0800

    init

---
+++ demo.sh
@@ -0,0 +1,5 @@
+python inference.py --mouth_region_size=256 \
+    --source_video_path=./asserts/examples/ck.mp4 \
+    --source_openface_landmark_path=./asserts/examples/ck.csv \
+    --driving_audio_path=./asserts/examples/ck-10.wav \
+    --pretrained_clip_DINet_path=./asserts/clip_training_DINet_256mouth.pth  
