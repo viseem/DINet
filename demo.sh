1605ae2eba90643a45a61641c8cb3abc46aa31a3
chenkai <chenkaic4@gmail.com>
2023-08-10 17:10:58 +0800

    fix

--- demo.sh
+++ demo.sh
@@ -1,5 +1,5 @@
 python inference.py --mouth_region_size=256 \
-    --source_video_path=./asserts/examples/ck.mp4 \
-    --source_openface_landmark_path=./asserts/examples/ck.csv \
-    --driving_audio_path=./asserts/examples/ck-10.wav \
+    --source_video_path=./asserts/examples/mm_2_xiao.mp4 \
+    --source_openface_landmark_path=./asserts/examples/mm_2_xiao.csv \
+    --driving_audio_path=./asserts/examples/cofi.wav \
     --pretrained_clip_DINet_path=./asserts/clip_training_DINet_256mouth.pth  
